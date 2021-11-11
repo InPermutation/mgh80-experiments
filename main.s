28c59201df2592e3eb4dd8529de3510dd6212f68
Jacob Krall <jacob@jkrall.net>
2021-11-10 22:03:55 -0500

    Return to SCM when input status present, use constants

--- main.s
+++ main.s
@@ -4,22 +4,39 @@ PORTB = $01
 PORTC = $02
 CONTROL_REGISTER = $03
 
+; SCM
+SCM_API = $30
+API_INPUT_STATUS = $03
+API_DELAY_MS = $0A
+
     .org $8000
     ld a,$80
     out (CONTROL_REGISTER),a
-    ld a, 0
+    ld a,0
 
 loop:
     out (PORTA),a
     inc a
     call delay
+
+.check_exit:
+    push af
+    ld c,API_INPUT_STATUS
+    rst SCM_API
+    jp nz,.exit
+    pop af
     jp loop
+.exit:
+    pop af
+    ld a,0
+    out (PORTA),a
+    ret
 
 delay:
     push af
     ld de,50
-    ld c, $0A ; Delay $DE ms
-    rst $30
+    ld c,API_DELAY_MS
+    rst SCM_API
     pop af
     ret
 
